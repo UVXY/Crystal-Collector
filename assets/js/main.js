3bf289a74b46a19183bb7551a8fc66cef584d5c5
ricky <ricky@rickys-MacBook-Air.local>
2018-09-03 19:26:59 -0700

    bs added, imgs added, basic layout

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+var randomNum;
+var lose;
+var win;
+
+// four crystals displayed as buttons(maybe)
+// rand number at the start of the game (19-120)
+// when crystal clicked, points will be added to total score (value 1-12)
+// player wins if score matches the total rand number 
+// player looses if score goes above the rand number 
+// game restarts when lose/win
+// show number of games player wins/looses
